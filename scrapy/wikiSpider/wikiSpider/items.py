76dc7e97681de5c73c95122c4294597a567771ad
Zaatar X <fadlallah8@gmail.com>
2022-01-07 15:19:38 -0300

    Third Commit: started using Scrapy but regex not working properly

---
+++ scrapy/wikiSpider/wikiSpider/items.py
@@ -0,0 +1,14 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+import scrapy
+
+
+class Article(scrapy.Item):
+    # define the fields for your time here like:
+    # name = scrapy.Field()
+    url = scrapy.Field()
+    title = scrapy.Field()
+    text = scrapy.Field()
+    lastUpdated = scrapy.Field()
